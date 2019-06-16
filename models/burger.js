1d5f022b0e1d81eda58c8d1851985f6f03d60cbf
tuequach <tuequach@gmail.com>
2019-06-16 14:30:39 -0500

    creating a sequelize model for burger based on the name of the burger and devoured

---
+++ models/burger.js
@@ -0,0 +1,14 @@
+module.exports = function (sequelize, Datatypes){
+    var Burger = sequelize.define('burgers', {
+        name: {
+            type: Datatypes.STRING,
+            allowNull: false,
+    }, 
+        devoured: {
+            type: Datatypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
+    });
+    return Burger;
+}
