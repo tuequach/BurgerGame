305c298b0b388b5df45540e09a52ef84f861abad
tuequach <tuequach@gmail.com>
2019-06-16 15:03:34 -0500

    route for updating burgers devoured

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -18,3 +18,15 @@ module.exports = function(app) {
     });
 });
 
+app.put('/:id', function(req, res) {
+    db.Burger.update({
+        devoured: true
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    }).then(function(dbBurger) {
+        res.json(dbBurger);
+    });
+});
