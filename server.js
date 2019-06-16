c66547437a80244dbf93eb46b57590e853f26ca0
tuequach <tuequach@gmail.com>
2019-06-16 15:13:45 -0500

    data parsing utilizing express and having a static directory

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ var PORT = process.env.PORT || 8080;
 
 var db = require('./models');
 
+//express for data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//static directory
+app.use(express.static('public'));
+
+
+
